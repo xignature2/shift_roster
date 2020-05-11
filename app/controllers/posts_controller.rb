226e06ae16bdf5dd4710216924819e95751810e7
xignature2 <58798583+xignature2@users.noreply.github.com>
2020-05-11 22:45:25 +0900

    実装途中

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def new
+  end
+
+  def creat
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
